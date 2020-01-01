f28eb1ec4bef88e7fc94d96aecc1942c33f556b1
Bruno Bertolini <dev@brunobertolini.com>
2020-01-01 19:52:19 -0300

    feat: add token expiration

--- setup/jwt.js
+++ setup/jwt.js
@@ -2,5 +2,5 @@ import jwt from 'jsonwebtoken'
 
 const secret = 'iIsInR5cCI6IkpXVCJ9eyJhbGciOiJIUzI1N'
 
-export const sign = payload => jwt.sign(payload, secret)
+export const sign = payload => jwt.sign(payload, secret, { expiresIn: 86400 })
 export const verify = token => jwt.verify(token, secret)
